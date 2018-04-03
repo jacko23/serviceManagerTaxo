e49af13afc0309c0ff0dcb0cf8b53a66e9ded033
Jackson <jackson.freire@piksel.com>
2018-04-03 10:27:21 +0200

    Cross Origen test config

--- src/main/java/taxoservice/taxoservice/controller/StartProjectController.java
+++ src/main/java/taxoservice/taxoservice/controller/StartProjectController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import taxoservice.taxoservice.services.ContactService;
 
 
 @RestController
+@CrossOrigin(origins = "https://taxoservice.herokuapp.com/")
 @RequestMapping("/contacts")
 public class StartProjectController {
 	
